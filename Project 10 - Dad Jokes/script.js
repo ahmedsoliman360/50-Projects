ddcd68fe30ca60e2e1efff082f60a516d199105a
Ahmed Soliman <ahmedsoliman360@gmail.com>
2021-09-06 11:09:25 +0400

    Using await instead of .then to see which I like more

--- Project 10 - Dad Jokes/script.js
+++ Project 10 - Dad Jokes/script.js
@@ -3,17 +3,30 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-function generateJoke() {
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//    // Using fetch which is async with .then syntax kinda messy
+//   fetch("https://icanhazdadjoke.com/", config)
+//    // takes respons makes it to json which becomes the data from which we extract the joke
+//     .then((res) => res.json())
+//     .then((data) => {
+//     //   console.log(data);
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
+
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
-  fetch("https://icanhazdadjoke.com/", config)
-    .then((res) => res.json())
-    .then((data) => {
-      //   console.log(data);
-      jokeEl.innerHTML = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+  const data = res.json();
+  jokeEl.innerHTML = data.joke;
 }
 jokeBtn.addEventListener("click", generateJoke);
